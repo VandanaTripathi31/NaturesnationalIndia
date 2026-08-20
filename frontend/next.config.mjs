458630ed629b15ad6e4bd8daf6d5358c176ccb96
Vandana Tripathi <tripathivandana086@gmail.com>
2026-08-20 18:27:30 +0530

    file changed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,3 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -30,9 +24,6 @@ const nextConfig = {
         hostname: "*.naturesnaturalindia.com",
       },
     ],
-  },
-  turbopack: {
-    root: __dirname,
   },
 };
 
