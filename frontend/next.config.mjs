e679dfede5099ce753fc66ceae8cfe8c6bb458ab
Vandana Tripathi <tripathivandana086@gmail.com>
2026-08-21 01:06:59 +0530

    changes fixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,28 +2,26 @@
 const nextConfig = {
   output: "standalone",
   compress: false,
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable, no-transform",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.naturesnaturalindia.com",
-      },
-      {
-        protocol: "https",
-        hostname: "naturesnaturalindia.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.naturesnaturalindia.com",
-      },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "www.naturesnaturalindia.com" },
+      { protocol: "https", hostname: "naturesnaturalindia.com" },
+      { protocol: "https", hostname: "*.naturesnaturalindia.com" },
     ],
   },
 };
